ef0776cd9bd1b591959ac103c8c0266aaafab165
OKder <cooky73200@gmail.com>
2021-08-22 15:08:22 +0800

    Third time

--- src/main/java/tw/iii/action/WorkEx1.java
+++ src/main/java/tw/iii/action/WorkEx1.java
@@ -2,11 +2,9 @@ package tw.iii.action;
 
 public class WorkEx1 {
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("操棒棒的阿");
-=======
-		System.out.println("好好棒棒喔");
->>>>>>> branch 'master' of git@github.com:cooky73200/Demo1.git
+
+		System.out.println("好好棒棒喔的阿");
+
 	}
 
 }
